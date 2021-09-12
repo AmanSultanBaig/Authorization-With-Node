540bc458657e54b2a48977b8d46cbf171f161280
Aman Sultan Baig <amansultanbaig@gmail.com>
2021-09-12 14:28:47 +0500

    basic authorization done

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("Authorization with Node")
+const express = require('express');
+const app = express();
+
+const app_routes = require("./routes/routes")
+
+app.use(express.json())
+app.use(app_routes)
+
+let port = 5050 || process.env.PORT
+
+app.listen(port, () => console.log('App is Running...'))
